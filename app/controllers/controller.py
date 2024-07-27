be0b4058503eb6cdf359d8b75a2333fb4e03db0e
Võ Khánh An <vokhanhan24@gmail.com>
2024-07-27 19:36:01 +0700

    Model vs Controller: insert quanhuyen, tinhtp, xaphuong, getall usersbyhoten, users, capnhatphanquyenbyidus, quanhuyenbyidquan, tinhtpbyidtinh, userbyidus,, vaitrobyidvt, matkhau, xóa taikhoan, phanquyen, tinh, xaphuong

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -11,12 +11,20 @@ def insert_vai_tro(idvt: int, tenvaitro: str) -> bool:
     result = insert_vai_tro(idvt, tenvaitro)
     return result
 
-def insert_phan_quyen(idus: int, idvt: int, trangthai: bool, ghichu: str) -> bool:
-    result = insert_phan_quyen(idus, idvt, trangthai, ghichu)
-    return result
-
 def insert_tai_khoan(idtk: int, taikhoan: str, matkhau: str, ngaytao: datetime, ngaycapnhat: datetime, trangthai: bool) -> bool:
     result = insert_tai_khoan(idtk, taikhoan, matkhau, ngaytao, ngaycapnhat,  trangthai)
+    return result
+
+def insert_quan_huyen(idquan: str, tenqh: str, idtinh: str ) -> bool:
+    result = insert_quan_huyen(idquan, tenqh, idtinh)
+    return result
+
+def insert_tinh_tp(idtinh: str, ten: str) -> bool:
+    result = insert_tinh_tp(idtinh, ten)
+    return result
+    
+def insert_xa_phuong(idxa: str, tenxp: str, idquan: str) -> bool:
+    result = insert_xa_phuong(idxa, tenxp, idquan)
     return result
 
 def get_all_phan_quyen_controller():
@@ -28,25 +36,52 @@ def get_all_vai_tro_controller():
 def get_all_tai_khoan_controller():
     return get_all_tai_khoan()
 
-def get_all_dmtaikhoan_by_tai_khoan_controller(taikhoan: str):
-    return get_all_dmtaikhoan_by_tai_khoan(taikhoan)
+def get_all_users_controller():
+    return get_all_users()
 
-def update_phan_quyen_by_id_controller(idus: int, idvt: int, trangthai: bool, ghichu: str):
+def get_all_danh_muc_tai_khoan_by_tai_khoan_controller(taikhoan: str):
+    return get_all_danh_muc_tai_khoan_by_tai_khoan(taikhoan)
+
+def get_all_users_by_ho_ten_controller(hoten: str):
+    return get_all_users_by_ho_ten(hoten)
+
+def update_phan_quyen_by_idus_controller(idus: int, idvt: int, trangthai: bool, ghichu: str):
     return update_phan_quyen_by_id(idus, idvt, trangthai, ghichu)
 
-def update_vai_tro_by_id_controller(idvt: int, tenvaitro: str):
+def update_vai_tro_by_idvt_controller(idvt: int, tenvaitro: str):
     return update_vai_tro_by_id(idvt, tenvaitro)
+
+def capnhat_quan_huyen_by_idquan_controller(idquan: str, tenqh: str, idtinh: str):
+    return capnhat_quan_huyen_by_idquan(idquan, tenqh, idtinh)
+
+def capnhat_tinh_tp_by_idtinh_controller(idtinh: str, ten: str):
+    return capnhat_tinh_tp_by_idtinh(idtinh, ten)
+
+def capnhat_users_by_idus_controller(idus: str, idpb: str,idtk:str, hoten: str, ngaysinh: datetime, diachi: str, sodienthoai: str, email: str, gioitinh: str, ghichu: str, chucvu: str):
+    return_users_by_idus(idus, idpb,idtk, hoten, ngaysinh, diachi, sodienthoai, email, gioitinh, ghichu, chucvu)
+
+def capnhat_xa_phuong_by_idxa_controller(idxa: str, tenxp: str, idquan: str):
+    return capnhat_xa_phuong_by_idxa(idxa, tenxp, idquan)
 
 def update_matkhau_controller(request: UpdatePasswordRequest):
     return update_matkhau(request)
 
-def xoa_phan_quyen_by_idus_controller(idus: int):
-    return xoa_phan_quyen_by_idus(idus)
+def update_xoa_users_by_idus_controller(idus: str):
+    return update_xoa_users_by_idus(idus)
 
-def xoa_tai_khoan_by_idtk_controller(idtk: int):
+def update_xoa_quan_huyen_by_idquan_controller(idquan: str):
+    return update_xoa_quan_huyen_by_idquan(idquan)
+
+def update_xoa_tinh_by_idtinh_controller(idtinh: str):
+    return update_xoa_tinh_by_idtinh(idtinh)
+
+def update_xoa_xa_phuong_by_idxa_controller(idxa: str):
+    returnupdate_xoa_xa_phuong_by_idxa(idxa)
+
+def update_xoa_tai_khoan_by_idtk_controller(idtk: int):
     return xoa_tai_khoan_by_idtk(idtk)
 
-def xoa_vai_tro_by_idvt_controller(idvt: int):
+def update_xoa_vai_tro_by_idvt_controller(idvt: int):
     return xoa_vai_tro_by_idvt(idvt)
 
 def create_account_controller(request: CreateAccountRequest):
--- app/models/models.py
+++ app/models/models.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 
 # bảng công ty
 
-def insert_user(idus: str, idpb: str,idtk:str, hoten: str, ngaysinh: datetime, diachi: str, sodienthoai: str, email: str, gioitinh: str, ghichu: str, chucvu: str,) -> bool:
+def insert_user(idus: str, idpb: str,idtk:str, hoten: str, ngaysinh: datetime, diachi: str, sodienthoai: str, email: str, gioitinh: str, ghichu: str, chucvu: str) -> bool:
     try:
         i = cursor.execute("EXEC InsertUser ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?", idus, idpb,idtk, hoten, ngaysinh, diachi, sodienthoai, email, gioitinh, ghichu, chucvu).fetchone()
         result = i[0]
@@ -24,6 +24,37 @@ def insert_vai_tro(idvt: str, tenvt: str, trangthai: bool, ghichu: str, ) -> boo
     except Exception as e:
         print(e)
         return False
+    
+def insert_quan_huyen(idquan: str, tenqh: str, idtinh: str ) -> bool:
+    try:
+        i = cursor.execute("EXEC InsertUser ?, ?, ?", idquan, tenqh, idtinh).fetchone()
+        result = i[0]
+        conn.commit()
+        return result
+    except Exception as e:
+        print(e)
+        return False
+    
+def insert_tinh_tp(idtinh: str, ten: str) -> bool:
+    try:
+        i = cursor.execute("EXEC InsertUser ?, ?", idtinh, ten).fetchone()
+        result = i[0]
+        conn.commit()
+        return result
+    except Exception as e:
+        print(e)
+        return False
+    
+def insert_xa_phuong(idxa: str, tenxp: str, idquan: str) -> bool:
+    try:
+        i = cursor.execute("EXEC InsertUser ?, ?, ?", idxa, tenxp, idquan).fetchone()
+        result = i[0]
+        conn.commit()
+        return result
+    except Exception as e:
+        print(e)
+        return False
+    
 def insert_tai_khoan(idtk:str, taikhoan: str, matkhau: str, ngaytao: datetime, ngaycapnhat: datetime, ) -> bool:
     try:
         i = cursor.execute("EXEC InsertUser ?, ?, ?, ?", idvt, tenvt, trangthai, ghichu).fetchone()
@@ -43,6 +74,15 @@ def verify_tai_khoan(taikhoan: str, matkhau: str):
         return True
     except Exception as e:
         return e
+
+def get_all_users_by_ho_ten(hoten: str):
+    try:
+        result = cursor.execute("EXEC GetusersByhoten ?", hoten).fetchall()
+        data = [{'idus': i[0], 'idpb': i[1], 'hoten': i[2], 'ngaysinh': i[3], 'diachi': i[4], 'sodienthoai': i[5], 'emai': i[6], 'gioitinh': i[7], 'chucvu': i[8], 'trangthai': i[9]} for i in result]
+        return data
+    except Exception as e:
+        return e
+    
 def get_all_danh_muc_tai_khoan_by_tai_khoan(taikhoan: str):
     try:
         result = cursor.execute("EXEC GetDanhMucTaiKhoanByTaiKhoan ?", id).fetchall()
@@ -50,6 +90,14 @@ def get_all_danh_muc_tai_khoan_by_tai_khoan(taikhoan: str):
         return data
     except Exception as e:
         return e
+    
+def get_all_users():
+    try:
+        result = cursor.execute("EXEC GetDSVTDashboard").fetchall()
+        return result
+    except Exception as e:
+        return e
+
 def get_all_vai_tro():
     try:
         result = cursor.execute("EXEC GetDSVTDashboard").fetchall()
@@ -68,44 +116,103 @@ def get_all_tai_khoan():
         return result
     except Exception as e:
         return e
-def capnhat_phan_quyen(idus: str, idvt: str, trangthai: bool, ghichu:str):
+def capnhat_phan_quyen_by_idus(idus: str, idvt: str, trangthai: bool, ghichu:str):
     try:
-        result = cursor.execute("EXEC InsertKyThucTap ?, ?, ?", idus, idvt, trangthai,ghichu)
+        result = cursor.execute("EXEC Insertphanquyen ?, ?, ?", idus, idvt, trangthai,ghichu)
         conn.commit()
         return True
     except Exception as e:
         return e
-def capnhat_vai_tro(idvt: str, tenvt: str, trangthai: bool, ghichu:str):
+    
+def capnhat_quan_huyen_by_idquan(idquan: str, tenqh: str, idtinh: str):
     try:
-        result = cursor.execute("EXEC InsertKyThucTap ?, ?, ?", idvt, tenvt, trangthai,ghichu)
+        result = cursor.execute("EXEC Insertquanhuyen ?, ?, ?", idquan, tenqh, idtinh)
         conn.commit()
         return True
     except Exception as e:
         return e
-def doi_mk(request: UpdatePasswordRequest):
+
+def capnhat_tinh_tp_by_idtinh(idtinh: str, ten: str):
+    try:
+        result = cursor.execute("EXEC Inserttinh ?, ?", idtinh, ten)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+
+def capnhat_users_by_idus(idus: str, idpb: str,idtk:str, hoten: str, ngaysinh: datetime, diachi: str, sodienthoai: str, email: str, gioitinh: str, ghichu: str, chucvu: str):
+    try:
+        result = cursor.execute("EXEC InsertUser ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?", idus, idpb,idtk, hoten, ngaysinh, diachi, sodienthoai, email, gioitinh, ghichu, chucvu)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+    
+def capnhat_xa_phuong_by_idxa(idxa: str, tenxp: str, idquan: str):
+    try:
+        result = cursor.execute("EXEC InsertUser ?, ?, ?", idxa, tenxp, ididquan)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+
+def capnhat_vai_tro_by_idvt(idvt: str, tenvt: str, trangthai: bool, ghichu:str):
+    try:
+        result = cursor.execute("EXEC Insertvaitro ?, ?, ?", idvt, tenvt, trangthai,ghichu)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+def update_matkhau(request: UpdatePasswordRequest):
     try:
         result = cursor.execute("EXEC UpdateMatKhau ?, ?",  request.taikhoan, request.new_password)
         conn.commit()
         return True
     except Exception as e:
         return e
-"""def update_xoa_phan_quyen_by_idus(idus: str):
-    try:
-        result = cursor.execute("EXEC UpdateXoaPhanQuyenByIDUS ?", id)
-        conn.commit()
-        return True
-    except Exception as e:
-        return e"""
+
 def update_xoa_tai_khoan_by_idtk(idtk: str):
     try:
-        result = cursor.execute("EXEC UpdateXoaTaiKhoanByIDTK ?", id)
+        result = cursor.execute("EXEC UpdateXoaTaiKhoanByIDTK ?", idtk)
         conn.commit()
         return True
     except Exception as e:
         return e
+def update_xoa_users_by_idus(idus: str):
+    try:
+        result = cursor.execute("EXEC UpdateXoausersbyidus ?", idus)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+
+def update_xoa_quan_huyen_by_idquan(idquan: str):
+    try:
+        result = cursor.execute("EXEC UpdateXoaquanhuyenbyidquan ?", idquan)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+    
+def update_xoa_tinh_by_idtinh(idtinh: str):
+    try:
+        result = cursor.execute("EXEC Updatexoatinhbyidtinh ?", idtinh)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+    
+def update_xoa_xa_phuong_by_idxa(idxa: str):
+    try:
+        result = cursor.execute("EXEC Updatexoaxaphuongbyidxa ?", idxa)
+        conn.commit()
+        return True
+    except Exception as e:
+        return e
+
 def update_xoa_vai_tro_by_idvt(idvt: str):
     try:
-        result = cursor.execute("EXEC UpdateXoaVaiTroByIDVT ?", id)
+        result = cursor.execute("EXEC UpdateXoaVaiTroByIDVT ?", idvt)
         conn.commit()
         return True
     except Exception as e:
